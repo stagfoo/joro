43bf3a9d5f85307acd9ad4ee36b02e582f429528
stagfoo <alexanderddking@gmail.com>
2022-02-18 09:24:34 +0900

    🍱　refactor ░ add strict typing

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,64 @@
-function joro(){
-    this.registry = {}
-    this.getElement = (key:string) => {
+type CustomTag = HTMLElement | null;
+interface RegistryItem {
+  [x: string]: CustomTag
+}
+
+class joro {
+  [x: string]: any; //TODO fix me
+  render: (key: string, custom: CustomTag) => boolean;
+  getElement: (key: string) => Element | null;
+  add: (key: string, inner: string, custom: CustomTag, render: boolean) => boolean;
+  isNotRendered:(key: string) => boolean;
+  remove: (key: string, unrender: boolean) => boolean;
+  
+  constructor() {
+    this.registry = <RegistryItem>{}
+    this.getElement = (key: string) => {
       return document.querySelector(`[data-joro-id=${key}]`)
     }
-    this.render = (key:string, custom:HTMLElement = null) => {
+    this.render = (key: string, custom: CustomTag = null) => {
       if (this.isNotRendered(key)) {
         //Get the data that should be stored in the key
-        const inner = this.registry[key];
-        if(custom instanceof HTMLElement) {
-            //html auto converts camel-case to dash case
-            custom.dataset.joroId = key;
-            custom.innerHTML = inner;
-            document.head.appendChild(custom);
+        const inner:string = this.registry[key];
+        if (custom instanceof HTMLElement) {
+          //html auto converts camel-case to dash case
+          custom.dataset.joroId = key;
+          custom.innerHTML = inner;
+          document.head.appendChild(custom);
+          return true;
         }
       }
+      return false;
     },
-    this.add = (key:string, inner:string, custom: HTMLElement = null, render:boolean = true) => {
-      if(this.registry[key] === undefined){
+      this.add = (key: string, inner: string, custom: CustomTag = null, render: boolean = true) => {
+        if (this.registry[key] === undefined) {
           this.registry[key] = inner;
           if (render) {
             this.render(key, custom);
+            return true;
           }
+        }
+        return false
       }
-    }
-    this.remove = (key: string, unrender:boolean = false) => {
+    this.remove = (key: string, unrender: boolean = false) => {
       delete this.registry[key];
-      if(unrender && !this.isNotRendered(key)){
-        this.getElement(key).remove()
+      if (unrender && !this.isNotRendered(key)) {
+        this.getElement(key)?.remove()
+        return true
       }
+      return false;
+    }
+
+    this.isNotRendered = (key: any) => {
+      return this.getElement(key) == null;
+    }
   }
 
-  this.isNotRendered = (key) => {
-    return this.getElement(key) == null;
-  }
+
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports = joro;
+  module.exports = joro;
 } else {
-	window['joro'] = joro;
+  (window as any).joro = joro;
 }
