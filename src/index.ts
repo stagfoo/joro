282b504c8e83af50cb78c89bc7463a5de38c774b
stagfoo <alexanderddking@gmail.com>
2022-02-23 10:01:25 +0900

    🪴　feature ░ add test: add()

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,24 @@ interface Registry {
   [x: string]: string;
 }
 
+type Add = (
+  _key: string,
+  _inner: string,
+  _custom: CustomTag,
+  _render: boolean,
+  _location: 'head' | 'body',
+) => boolean;
+
+type render =  (_key: string, _custom: CustomTag, _location: 'head' | 'body') => boolean;
+
+
+
 type Joro = {
   new (): {
     registry: Registry;
-    render: (_key: string, _custom: CustomTag) => boolean;
+    render: render
     getElement: (_key: string) => Element | null;
-    add: (
-      _key: string,
-      _inner: string,
-      _custom: CustomTag,
-      _render: boolean
-    ) => boolean;
+    add: Add
   };
 }
 
@@ -24,15 +31,9 @@ interface Window {
 
 class joro {
   registry: Registry;
-  render: (_key: string, _custom: CustomTag) => boolean;
   getElement: (_key: string) => Element | null;
-  add: (
-    _key: string,
-    _inner: string,
-    _custom: CustomTag,
-    _render: boolean
-  ) => boolean;
-
+  render: render
+  add: Add
   isNotRendered: (_key: string) => boolean;
   remove: (_key: string, _unrender: boolean) => boolean;
 
@@ -40,37 +41,40 @@ class joro {
     this.registry = {};
     this.getElement = (key: string) =>
       document.querySelector(`[data-joro-id=${key}]`);
-    (this.render = (key: string, custom: CustomTag = null) => {
+    (this.render = (key: string, tag: CustomTag = null, location = "head") => {
       if (this.isNotRendered(key)) {
         // Get the data that should be stored in the key
         const inner: string = this.registry[key];
-        if (custom instanceof HTMLElement) {
+        if (tag instanceof HTMLElement) {
           // Html auto converts camel-case to dash case
-          custom.dataset.joroId = key;
-          custom.innerHTML = inner;
-          document.head.appendChild(custom);
+          tag.dataset.joroId = key;
+          tag.innerHTML = inner;
+          document[location].appendChild(tag);
           return true;
         }
+        console.error("ヾ( `3´)シ GET: tag is not an instanceof HTMLElement");
       }
-
+      console.error("ヾ( `3´)シ GET: That tag is not rendered");
       return false;
     });
 
     (this.add = (
       key: string,
       inner: string,
-      custom: CustomTag = null,
+      tag: CustomTag = null,
       render = true,
+      location = "head"
     ) => {
       if (this.registry[key] === undefined) {
         this.registry[key] = inner;
         if (render) {
-          this.render(key, custom);
-          return true;
+          this.render(key, tag, location);
         }
+      } else {
+        console.error("ヾ( `3´)シ ADD: That key already exists");
+        return false;
       }
-
-      return false;
+      return true;
     });
 
     this.remove = (key: string, unrender = false) => {
@@ -79,7 +83,7 @@ class joro {
         this.getElement(key)?.remove();
         return true;
       }
-
+      console.error("ヾ( `3´)シ REMOVE: That key is not rendered");
       return false;
     };
 
